9bd6b3d90f8852fca8cc86db463f7071ee2827d4
Krzysztof Kaptur <krzysztof.kaptur@gmail.com>
2024-08-23 18:07:26 +0200

    17-2-custom-actions-js

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core')
 const exec = require('@actions/exec')
+const github = required('@actions/github')
 
 const validateBranchName = ({ branchName }) =>
   /^[a-zA-Z0-9_\-\.\/]+$/.test(branchName)
@@ -8,11 +9,15 @@ const validateDirectoryName = ({ dirName }) =>
   /^[a-zA-Z0-9_\-\/]+$/.test(dirName)
 
 async function run() {
-  const baseBranch = core.getInput('base-branch')
-  const targetBranch = core.getInput('target-branch')
-  const ghToken = core.getInput('gh-token')
-  const workingDir = core.getInput('working-directory')
+  const baseBranch = core.getInput('base-branch', { required: true })
+  const targetBranch = core.getInput('target-branch', { required: true })
+  const ghToken = core.getInput('gh-token', { required: true })
+  const workingDir = core.getInput('working-directory', { required: true })
   const debug = core.getBooleanInput('debug')
+
+  const commonExecOpts = {
+    cwd: workingDir
+  }
 
   core.setSecret(ghToken)
 
@@ -41,20 +46,53 @@ async function run() {
   core.info(`[js-dependency-update] : target branch is ${targetBranch}`)
   core.info(`[js-dependency-update] : working directory is ${workingDir}`)
 
-  await exec.exec('npm update', [], {
-    cwd: workingDir
-  })
+  await exec.exec('npm update', [], commonExecOpts)
 
   const gitStatus = await exec.getExecOutput(
     'git status -s package*.json',
     [],
-    {
-      cwd: workingDir
-    }
+    commonExecOpts
   )
 
   if (gitStatus.stdout.length > 0) {
     core.info('[js-dependency-update] : There are updates available!')
+    await exec.exec(`git config --global user.name "gh-automation"`)
+    await exec.exec(`git config --global user.email "gh-automation@email.com"`)
+    await exec.exec(`git checkout -b ${targetBranch}`, [], commonExecOpts)
+    await exec.exec(
+      `git add package.json package-lock.json`,
+      [],
+      commonExecOpts
+    )
+    await exec.exec(
+      `git commit -m "chore: update dependencies"`,
+      [],
+      commonExecOpts
+    )
+    await exec.exec(
+      `git push -u origin ${targetBranch} --force`,
+      [],
+      commonExecOpts
+    )
+
+    const oktokit = github.getOctokit(ghToken)
+
+    try {
+      await oktokit.rest.pulls.create({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        title: 'Update npm dependencies',
+        body: 'This pull request updates npm packages',
+        base: baseBranch,
+        head: targetBranch
+      })
+    } catch (e) {
+      core.error(
+        '[js-dependency-update]: Something went wrong while creating the PR. Check logs below'
+        core.setFailed(e.message)
+        core.error(e)
+      )
+    }
   } else {
     core.info('[js-dependency-update] : No updates at this point in time!')
   }
